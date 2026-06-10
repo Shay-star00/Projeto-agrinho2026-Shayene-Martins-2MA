902e016daaaf61e2543cfb06fe1cdc7e96225388
Shay-star00 <martins.shayene@escola.pr.gov.br>
2026-06-10 08:21:37 -0300

    ble

--- scrip.js
+++ scrip.js
@@ -1,8 +1,3 @@
-// Arquivo JavaScript - script.js
-// O código original não tinha JavaScript, então está vazio.
-// Adicione suas funções JavaScript aqui quando precisar.
-
-// Exemplo: função para voltar ao topo com animação
 document.querySelector('.floating').addEventListener('click', function(e) {
   e.preventDefault();
   window.scrollTo({
